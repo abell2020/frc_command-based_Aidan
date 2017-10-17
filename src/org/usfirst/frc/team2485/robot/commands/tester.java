55a4c374edf1b49a697f9f50054e9e91ca118489
aidan <abell2020@francisparker.org>
2017-10-17 16:02:27 -0700

    created repo

---
+++ src/org/usfirst/frc/team2485/robot/commands/tester.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team2485.robot.commands;
+
+import edu.wpi.first.wpilibj.command.InstantCommand;
+
+public class tester extends InstantCommand{
+
+	public tester(){
+		System.out.println("stuff happened");
+	}
+	public void execute() {
+		System.out.println("yay");
+	}
+}
